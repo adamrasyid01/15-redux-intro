c8d18d79c88ee169d6d56e822e10b77b56c81d1a
Adam Rasyid N <adamrasyid01@gmail.com>
2026-06-11 16:41:29 +0700

    Create store with reduxjs/toolkit

--- src/store.js
+++ src/store.js
@@ -1,15 +1,14 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 // Daftarin Reducer ke rootReducer, nanti rootReducer yang akan kita daftarkan ke store
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
